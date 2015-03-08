0977cd4278a56192dd42235ac34ce4a0334a673b
Gaye Bulut <gayegul@gmail.com>
2015-03-07 16:12:29 -0800

    missing semicolon

--- test/simpleHttpServerTest.js
+++ test/simpleHttpServerTest.js
@@ -10,7 +10,7 @@ chai.use(chaihttp);
 var expected = '{ "idNum" : "test" }';
 var getRandResName = function() {
 	return '/magic/' + (10000 + Math.random() * 10000);
-}
+};
 
 describe('simple http server w persistance', function() {
 
